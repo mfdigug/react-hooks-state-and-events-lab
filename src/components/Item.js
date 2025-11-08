b1ced47de68d729b3c1480e362843c510e2a8497
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-11-08 17:15:43 +1100

    updated list item button class and text

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+  
+  function handleClick(){
+    setInCart(!inCart)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : "add" }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{inCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
